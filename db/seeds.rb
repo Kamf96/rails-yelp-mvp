0e77e06307975aef3ef6c6ac4c4a9486078d6cda
kenneth morante <kennethprime96@hotmail.com>
2022-01-20 00:22:51 -0500

    para completar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating restaurants..."
+chicarroneria = { name: 'Cecilia', category: 'chinese', address: 'Calle Arancota 100' }
+pizza = {  name: 'Presto', category: 'italian', address: 'Calle mercaderes 101' }
+quequita = { name: 'Carlo', category: 'italian', address: 'Av. lima' }
+chifa = { name: 'Wok', category: 'chinese', address: 'Calle octavio 400' }
+algo = { name: 'Rico', category: 'french', address: 'Calle tulipanes 100' }
+
+[chicarroneria, pizza, quequita, chifa, algo].each do |restaurant|
+  restaurantes = Restaurant.create!(restaurant)
+  puts "Created #{restaurantes.name}"
+end
+puts 'finished'
